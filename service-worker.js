e9315ed8e296934c7e629ba419de2698d3a2d681
Alex Telnov <telalpal@gmail.com>
2019-08-28 02:28:06 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7322b68a480108652be3e85c88a31dae.js"
+  "./precache-manifest.4b711168e1e193e999cb9ff94389fda8.js"
 );
 
 workbox.clientsClaim();
