e3890b679c98bf1bda80341ba91a6b8a95fceb4d
Alex Telnov <telalpal@gmail.com>
2020-06-16 21:54:43 +0300

    Updates

---
+++ precache-manifest.f1d25224e675819c8793ba6a178231a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e94e75b446ee45be0d44",
+    "url": "./static/css/main.6abdd9a2.chunk.css"
+  },
+  {
+    "revision": "e94e75b446ee45be0d44",
+    "url": "./static/js/main.81c01324.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6a673ac44a59f45e0fc8",
+    "url": "./static/css/2.00002bfa.chunk.css"
+  },
+  {
+    "revision": "6a673ac44a59f45e0fc8",
+    "url": "./static/js/2.d7dbf1a6.chunk.js"
+  },
+  {
+    "revision": "0d1882f31d058a053f450e2c2a99d278",
+    "url": "./index.html"
+  }
+];
