e4c374075965806e1e1d29a7c41c991f31885b5c
Alex Telnov <telalpal@gmail.com>
2020-06-17 04:02:04 +0300

    Updates

---
+++ precache-manifest.1df60f323ac52505f642e729ce2edd75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810bf7b79d32bdcc71154c9098f2a0d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db41b63aee31561a0ead",
+    "url": "./static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "d45d9b65f1a0dc77b544",
+    "url": "./static/css/main.4f16029c.chunk.css"
+  },
+  {
+    "revision": "db41b63aee31561a0ead",
+    "url": "./static/js/2.9b643288.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9b643288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45d9b65f1a0dc77b544",
+    "url": "./static/js/main.3ac11606.chunk.js"
+  },
+  {
+    "revision": "07cb604aa745b27d6ede",
+    "url": "./static/js/runtime-main.608bb691.js"
+  }
+]);
